912ec8fd813c8aea876e59cf7ff477dfbe78a54b
Thomas Schmidt <thomas@thsmdt.com>
2022-02-05 00:54:15 +0100

    refactor: add trace logging to thread context

--- src/thread/thread_context.c
+++ src/thread/thread_context.c
@@ -6,6 +6,7 @@
 #include <logger.h>
 
 bool thread_init(struct thread_context* context, void* function, void* argument) {
+    LOG_TRACE("Prepare initialization of new thread");
     int ret;
     context->child_context.thread_argument = argument;
     context->child_context.state = STARTED;
@@ -16,18 +17,24 @@ bool thread_init(struct thread_context* context, void* function, void* argument)
         return false;
     }
 
+    LOG_TRACE("Finished initialization of new thread");
     return true;
 }
 
 bool thread_signal_stop(struct thread_context* context) {
+    LOG_TRACE("Request thread to stop");
     context->child_context.state = STOP_REQUESTED;
 }
 
 bool thread_join(struct thread_context* context) {
+    LOG_TRACE("Joining thread");
     int ret = pthread_join(context->pthread_reference, NULL);
+    LOG_TRACE("Joined thread returned");
+    context->child_context.state = STOPPED;
     return ret == 0;
 }
 
 bool thread_deinit(struct thread_context* context) {
+    LOG_TRACE("De-initializing thread");
     pthread_cancel(context->pthread_reference);
 }
