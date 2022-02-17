88a8125c1d7bda5f0085e25b3015d5f5b6efc7cf
Thomas Schmidt <thomas@thsmdt.com>
2022-02-17 01:56:04 +0100

    fix: root-fs is treated separately

--- include/path_helper.h
+++ include/path_helper.h
@@ -6,5 +6,9 @@
 
 static inline size_t path_on_mountpoint(const char* path, const char* mountpoint) {
     size_t mountpoint_len = strlen(mountpoint);
+    if (mountpoint_len == 1 && mountpoint[0] == '/') {
+        // root fs
+        return 1;
+    }
     return strncmp(path, mountpoint, mountpoint_len) == 0 && path[mountpoint_len] == '/' ? mountpoint_len+1 : 0;
 }
