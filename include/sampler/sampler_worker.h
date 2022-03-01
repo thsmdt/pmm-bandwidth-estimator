b8e82a8d15fa7519a351924418017f3a6bbc7b21
Thomas Schmidt <thomas@thsmdt.com>
2022-03-02 00:52:15 +0100

    perf: increase runs of sampler per second

--- include/sampler/sampler_worker.h
+++ include/sampler/sampler_worker.h
@@ -2,7 +2,7 @@
 
 #include <sampler/sampler_context.h>
 
-#define SAMPLER_MAX_ITERATIONS_PER_SECOND 10
+#define SAMPLER_MAX_ITERATIONS_PER_SECOND 2000000
 
 void sampler_worker(void* argument);
 void sampler_worker_iteration(struct sampler_context *context);
